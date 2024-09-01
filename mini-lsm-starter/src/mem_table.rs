ec35367c66747a54c54a345b1db0d4cb2418d2c9
Ran Miller <ranmiller.leaf@gmail.com>
2024-09-01 19:35:59 +0800

    feat(starter): [week 1, day 1, task 1] SkipList Memtable

--- mini-lsm-starter/src/mem_table.rs
+++ mini-lsm-starter/src/mem_table.rs
@@ -37,8 +37,15 @@ pub(crate) fn map_bound(bound: Bound<&[u8]>) -> Bound<Bytes> {
 
 impl MemTable {
     /// Create a new mem-table.
-    pub fn create(_id: usize) -> Self {
-        unimplemented!()
+    ///
+    /// TODO: create -> new.
+    pub fn create(id: usize) -> Self {
+        MemTable {
+            map: Arc::new(SkipMap::new()),
+            wal: None,
+            id,
+            approximate_size: Arc::new(AtomicUsize::new(0)),
+        }
     }
 
     /// Create a new mem-table with WAL
@@ -68,17 +75,25 @@ impl MemTable {
     }
 
     /// Get a value by key.
-    pub fn get(&self, _key: &[u8]) -> Option<Bytes> {
-        unimplemented!()
+    ///
+    /// 按key来获取值.
+    pub fn get(&self, key: &[u8]) -> Option<Bytes> {
+        // 使用skipmap获取key: 失败map方法返回None值
+        self.map.get(key).map(|entry| entry.value().clone())
     }
 
     /// Put a key-value pair into the mem-table.
     ///
-    /// In week 1, day 1, simply put the key-value pair into the skipmap.
-    /// In week 2, day 6, also flush the data to WAL.
-    /// In week 3, day 5, modify the function to use the batch API.
-    pub fn put(&self, _key: &[u8], _value: &[u8]) -> Result<()> {
-        unimplemented!()
+    /// In week 1, day 1, simply put the key-value pair into the skipmap.<br>
+    /// In week 2, day 6, also flush the data to WAL.<br>
+    /// In week 3, day 5, modify the function to use the batch API.<br>
+    ///
+    /// 将键值对放到内存表中.<br>
+    /// done: 在第1周的第1天, 只需要将键值对放入skipmap中.<br>
+    pub fn put(&self, key: &[u8], value: &[u8]) -> Result<()> {
+        self.map
+            .insert(Bytes::copy_from_slice(key), Bytes::copy_from_slice(value));
+        Ok(())
     }
 
     /// Implement this in week 3, day 5.
